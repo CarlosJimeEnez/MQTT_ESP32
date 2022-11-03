4819062ede2c8515368e5215cce19d0a28cb2319
CarlosJimeEnez <jjimenezpena7@gmail.com>
2022-11-02 22:36:33 -0600

    A medias

---
+++ include/callback.h
@@ -0,0 +1,40 @@
+#ifndef Callback_function
+#define Callback_function
+#include <Arduino.h>
+#include <Motor.h>
+
+/* ------------- Funcion de llegada de los valores -------------------------- */
+void callback(char *topic, byte *payload, unsigned int length){
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    for (int i = 0; i < length; i++) {
+    Serial.print((char) payload[i]);
+    }
+
+    String topic_string = topic;
+
+    String concatenacion = ""; 
+    String auxiliar = "0"; 
+    for (size_t i = 0; i < length; i++)
+    {
+        auxiliar = String((char) payload[i]); 
+        concatenacion = concatenacion + auxiliar; 
+    }
+    int payload_int = concatenacion.toInt(); 
+    
+    Serial.println(payload_int);
+    Serial.println("-----------------------");
+    switch (expression)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
+
+
+#endif
